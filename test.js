8aed51296a8f0da03bf5d78cfc09cd560dfadb56
Sindre Sorhus <sindresorhus@gmail.com>
2025-09-15 05:04:53 +0700

    Require Node.js 20

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ test('main', t => {
 	t.regex('foo\u0000bar', filenameReservedRegex());
 	t.regex('foo\u001Fbar', filenameReservedRegex());
 	t.regex('foo*bar', filenameReservedRegex());
-	t.regex('foo\\bar', filenameReservedRegex());
+	t.regex(String.raw`foo\bar`, filenameReservedRegex());
 	t.regex('foo:bar', filenameReservedRegex());
 	t.notRegex('foo-bar', filenameReservedRegex());
 
